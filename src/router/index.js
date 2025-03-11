4fcee45fc6a58b9aa7db98b87e3b4964bf06cc65
SimoneColombo1 <simcolombo2@gmail.com>
2025-03-11 18:47:57 +0100

    Creata la pagina per il singolo progetto

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomePage.vue";
 import AllProjects from "../views/AllProjects.vue";
+import SingleProject from "../views/SingleProject.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/Projects",
       name: "AllProjects",
       component: AllProjects,
+    },
+    {
+      path: "/Projects/:id",
+      name: "SingleProject",
+      component: SingleProject,
     },
   ],
 });
--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -1,61 +1,72 @@
 <script setup>
+import { defineProps, watch } from "vue";
+import { useRoute } from "vue-router";
+
+// Ottieni l'ID dalla rotta
+const route = useRoute();
+const projectId = route.params.id;
+
+// Lista dei progetti
 const Projects = [
   {
-    id: 1,
+    ProjectId: 1,
     name: "PixelPortal",
     content:
       "Pixel Portal è un sito web per la visualizzazione e ricerca di video giochi",
     tecnologie: ["JavaScript", "NextJS", "HTML", "SCSS"],
   },
   {
-    id: 2,
+    ProjectId: 2,
     name: "BoolChef",
     content: "Portale per cercare e contattare chef",
     tecnologie: ["JavaScript", "Vue", "Laravel", "PHP", "HTML", "SCSS"],
   },
   {
-    id: 3,
+    ProjectId: 3,
     name: "Boolfolio",
     content:
-      "Portfolio con progetti all interno passati da un api creata da me",
+      "Portfolio con progetti all'interno passati da un'API creata da me",
     tecnologie: ["JavaScript", "Vue", "Laravel", "PHP", "HTML", "SCSS"],
   },
   {
-    id: 4,
+    ProjectId: 4,
     name: "BoolFlix",
     content:
-      "Riproduzione di un sito di streaming ispirata a netflix con l'utilizzo dell'api di imbd",
+      "Riproduzione di un sito di streaming ispirata a Netflix con l'uso dell'API di IMDB",
     tecnologie: ["JavaScript", "Vue", "HTML", "SCSS"],
   },
   {
-    id: 5,
+    ProjectId: 5,
     name: "BoolzApp",
-    content: "Riproduzione dell'interfaccia di una WebApp ispirata a WhatsApp ",
+    content: "Riproduzione dell'interfaccia di una WebApp ispirata a WhatsApp",
     tecnologie: ["JavaScript", "HTML", "SCSS"],
   },
 ];
-</script>
 
+let project = Projects.find((p) => p.ProjectId === parseInt(projectId));
+
+watch(
+  () => route.params.id,
+  (newId) => {
+    project = Projects.find((p) => p.ProjectId === parseInt(newId));
+  }
+);
+</script>
 <template>
-  <section class="main-container">
+  <section class="main-container" v-if="project">
     <section class="content">
-      <ul class="projects">
-        <li v-for="project in Projects">
-          <span>{{ project.name }}</span>
-          <span>{{ project.content }}</span>
-          <div class="technologies">
-            <span v-for="technology in project.tecnologie"
-              >{{ technology }}-</span
-            >
-            <router-link
-              :to="{ name: 'SingleProject', params: { id: project.id } }"
-              >dd</router-link
-            >
-          </div>
-        </li>
-      </ul>
+      <div class="projects">
+        <span>{{ project.name }}</span>
+        <span>{{ project.content }}</span>
+        <div class="technologies">
+          <span v-for="technology in project.tecnologie" :key="technology">
+            {{ technology }} -
+          </span>
+        </div>
+      </div>
     </section>
   </section>
+  <section class="main-container" v-else>progetto non trovato</section>
 </template>
 
 <style lang="scss" scoped>
